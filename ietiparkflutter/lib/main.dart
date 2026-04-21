455e46363899bc20e7b7acafb5684a78038b0be8
laurattt <lauratoro422@gmail.com>
2026-04-22 00:02:47 +0200

    minibase / arreglar

--- ietiparkflutter/lib/main.dart
+++ ietiparkflutter/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
+import 'widgets/game_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter WebSocket Demo',
+      title: 'Juego Cooperativo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Juego Online'),
+      home: const MyHomePage(title: 'Juego Cooperativo'),
     );
   }
 }
@@ -30,45 +31,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late WebSocketChannel channel;
-  String mensaje = "Esperando que inicie partida";
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Conexión al WebSocket
-    channel = WebSocketChannel.connect(Uri.parse('wss://pico2.ieti.site'));
-
-    // Escuchar mensajes del servidor
-    channel.stream.listen(
-      (data) {
-        setState(() {
-          mensaje = data.toString();
-        });
-      },
-      onError: (error) {
-        setState(() {
-          mensaje = "Error de conexión";
-        });
-      },
-      onDone: () {
-        setState(() {
-          mensaje = "Conexión cerrada";
-        });
-      },
-    );
-  }
-
-  @override
-  void dispose() {
-    channel.sink.close();
-    super.dispose();
-  }
-
-  void _enviarMensaje() {
-    channel.sink.add("Hola servidor");
-  }
+  bool _mostrarJuego = true;
 
   @override
   Widget build(BuildContext context) {
@@ -76,24 +39,42 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: Icon(_mostrarJuego ? Icons.visibility_off : Icons.visibility),
+            onPressed: () {
+              setState(() {
+                _mostrarJuego = !_mostrarJuego;
+              });
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              mensaje,
-              style: Theme.of(context).textTheme.headlineMedium,
-              textAlign: TextAlign.center,
+      body: _mostrarJuego
+          ? const GameWidget()
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(Icons.gamepad, size: 80, color: Colors.grey),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Vista oculta\nPresiona el botón de "ver" para mostrar el juego',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _mostrarJuego = true;
+                      });
+                    },
+                    child: const Text('Mostrar Juego'),
+                  ),
+                ],
+              ),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _enviarMensaje,
-              child: const Text("Enviar mensaje"),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
